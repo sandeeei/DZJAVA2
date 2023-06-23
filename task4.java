163f452508e049ee7e74ab9b9beed76680f66878
Sergei <sandeeey@example.com>
2023-06-23 17:39:19 +0300

    java4

---
+++ task4.java
@@ -0,0 +1,48 @@
+// 1) Дана строка sql-запроса "select * from students WHERE ". Сформируйте часть WHERE этого запроса, используя StringBuilder. Данные для фильтрации приведены ниже в виде json-строки.
+// Если значение null, то параметр не должен попадать в запрос.
+// Пример данной строки {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+// Вывод: select * from students WHERE name=Ivanov AND country=Russia AND city=Moscow
+package seminar2;
+
+/**
+ * task4
+ */
+ import java.io.BufferedReader;
+ import java.io.FileReader;
+ import java.io.IOException;
+// import java.util.Arrays;
+ 
+public class task4 {
+    public static void main(String[] args) throws Exception {
+      String [] list = lib.ReadLineFromFile("dataForSelect.txt"); 
+      System.out.println(list[0]); 
+      StringBuilder resultSelect = LineInList(list[0]);
+      System.out.println(resultSelect);
+    }
+    public static StringBuilder LineInList(String line) {
+        String line1 = line.replace("{", "");
+        String line2 = line1.replace("}", "");
+        String line3 = line2.replaceAll("\"", "");
+        System.out.println(line3);
+        StringBuilder result = new StringBuilder("select * from students where ");
+
+        String [] arrayData = line3.split(", ");
+        for (int i =0; i < arrayData.length; i++) {
+            String[] arrData = arrayData[i].split(":");
+            if(arrData[1].equals("null") == false) {
+                if (i != 0) {
+                    result.append(", ");
+                    result.append(arrData[0]);
+                    result.append(" = ");
+                    result.append(arrData[1]);
+                } else {
+                    result.append(arrData[0]);
+                    result.append(" = ");
+                    result.append(arrData[1]);
+                }
+            }
+            
+        }
+        return result;
+    }
+}
